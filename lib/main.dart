2ee41788f72e363750bc058c4af34676c0eba7c3
omzdmr <154471764+omzdmr@users.noreply.github.com>
2026-09-08 06:16:42 +0800

    Load settings before app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
+import 'src/settings/app_settings.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const QuranModernApp());
+  final settings = AppSettings();
+  await settings.load();
+  runApp(QuranModernApp(settings: settings));
 }
