ea50a930873f1286ddd70991af4d84851af53955
omzdmr <154471764+omzdmr@users.noreply.github.com>
2026-09-09 22:06:30 +0800

    Initialize background audio service at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+
 import 'src/app.dart';
+import 'src/features/audio/application/quran_audio_service.dart';
 import 'src/settings/app_settings.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final settings = AppSettings();
   await settings.load();
+  await QuranAudioService.instance.initialize();
   runApp(QuranModernApp(settings: settings));
 }
