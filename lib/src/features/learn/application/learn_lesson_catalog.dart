3a85465a801c62446da457783932a82c37cabe16
omzdmr <154471764+omzdmr@users.noreply.github.com>
2026-09-17 01:47:34 +0800

    Vary Learn quiz answer positions

--- lib/src/features/learn/application/learn_lesson_catalog.dart
+++ lib/src/features/learn/application/learn_lesson_catalog.dart
@@ -8,6 +8,7 @@ class LearnLessonDefinition {
     required this.introBodyKey,
     required this.summaryBodyKey,
     required this.quizDistractorSurahs,
+    required this.quizCorrectOptionIndex,
   });
 
   final String id;
@@ -18,6 +19,7 @@ class LearnLessonDefinition {
   final String introBodyKey;
   final String summaryBodyKey;
   final List<int> quizDistractorSurahs;
+  final int quizCorrectOptionIndex;
 
   int get firstAyah => ayahs.first;
 }
@@ -31,6 +33,7 @@ const LearnLessonDefinition inshirahEaseLesson = LearnLessonDefinition(
   introBodyKey: 'learnLessonEaseIntroBodyV1',
   summaryBodyKey: 'learnLessonEaseSummaryBodyV1',
   quizDistractorSurahs: <int>[93, 95],
+  quizCorrectOptionIndex: 1,
 );
 
 const LearnLessonDefinition asrLesson = LearnLessonDefinition(
@@ -42,6 +45,7 @@ const LearnLessonDefinition asrLesson = LearnLessonDefinition(
   introBodyKey: 'learnLessonAsrIntroBodyV1',
   summaryBodyKey: 'learnLessonAsrSummaryBodyV1',
   quizDistractorSurahs: <int>[102, 104],
+  quizCorrectOptionIndex: 2,
 );
 
 const LearnLessonDefinition ikhlasLesson = LearnLessonDefinition(
@@ -53,6 +57,7 @@ const LearnLessonDefinition ikhlasLesson = LearnLessonDefinition(
   introBodyKey: 'learnLessonIkhlasIntroBodyV1',
   summaryBodyKey: 'learnLessonIkhlasSummaryBodyV1',
   quizDistractorSurahs: <int>[111, 113],
+  quizCorrectOptionIndex: 0,
 );
 
 const List<LearnLessonDefinition> curatedLearnLessons = <LearnLessonDefinition>[
