cc0009c2e71bfe4210c8aef48893552aac3019ff
omzdmr <154471764+omzdmr@users.noreply.github.com>
2026-09-20 20:30:57 +0800

    test: remove temporary khatm compile probe

--- test/plans_screen_compile_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:quran_i_kerim/src/features/plans/plans_screen.dart';
-
-void main() {
-  test('plans screen compiles with khatm archive feature', () {
-    const screen = PlansScreen();
-    expect(screen, isA<PlansScreen>());
-  });
-}
