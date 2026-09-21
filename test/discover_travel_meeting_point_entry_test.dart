aa2d23af8e7cd192d17106b6a6a69fc6d116aacd
omzdmr <154471764+omzdmr@users.noreply.github.com>
2026-09-22 04:09:50 +0800

    Test Discover travel hub hierarchy

--- test/discover_travel_meeting_point_entry_test.dart
+++ test/discover_travel_meeting_point_entry_test.dart
@@ -4,20 +4,21 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:quran_i_kerim/src/features/discover/discover_screen.dart';
 
 void main() {
-  setUp(() {
-    SharedPreferences.setMockInitialValues(<String, Object>{});
-  });
+  setUp(() => SharedPreferences.setMockInitialValues(<String, Object>{}));
 
-  testWidgets('Discover opens offline travel meeting point', (tester) async {
+  testWidgets('Discover opens focused offline travel hub', (tester) async {
     await tester.pumpWidget(const MaterialApp(locale: Locale('tr'), home: Scaffold(body: DiscoverScreen())));
     await tester.pumpAndSettle();
 
-    await tester.scrollUntilVisible(find.text('Buluşma noktası'), 180, scrollable: find.byType(Scrollable).first);
-    expect(find.text('Seyahat listesi'), findsOneWidget);
-    await tester.tap(find.text('Buluşma noktası'));
+    await tester.scrollUntilVisible(find.text('Seyahat araçları'), 180, scrollable: find.byType(Scrollable).first);
+    await tester.tap(find.text('Seyahat araçları'));
     await tester.pumpAndSettle();
 
     expect(find.text('Buluşma noktası'), findsOneWidget);
+    expect(find.text('Seyahat listesi'), findsOneWidget);
+
+    await tester.tap(find.text('Buluşma noktası'));
+    await tester.pumpAndSettle();
     expect(find.text('Yer adı'), findsOneWidget);
     expect(find.text('Adres / tarif'), findsOneWidget);
   });
