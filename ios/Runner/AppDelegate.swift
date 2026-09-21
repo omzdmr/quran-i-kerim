c37e5d8cb8dc4440b4e9637e66669943068be11d
omzdmr <154471764+omzdmr@users.noreply.github.com>
2026-09-21 20:40:04 +0800

    ios: wire native lock-screen audio bridge

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,6 +5,7 @@ import UIKit
 @objc class AppDelegate: FlutterAppDelegate {
   private let audioSessionCoordinator = AudioSessionCoordinator.shared
   private var backupExclusionChannel: BackupExclusionChannel?
+  private var nowPlayingChannel: NowPlayingChannel?
 
   override func application(
     _ application: UIApplication,
@@ -12,24 +13,24 @@ import UIKit
   ) -> Bool {
     audioSessionCoordinator.start()
     GeneratedPluginRegistrant.register(with: self)
-    configureBackupExclusionChannel()
+    configureNativeChannels()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   override func applicationWillTerminate(_ application: UIApplication) {
+    nowPlayingChannel?.detach()
     backupExclusionChannel?.detach()
     audioSessionCoordinator.stop()
     super.applicationWillTerminate(application)
   }
 
-  private func configureBackupExclusionChannel() {
+  private func configureNativeChannels() {
     guard let controller = window?.rootViewController as? FlutterViewController else {
-      NSLog("Unable to install backup exclusion channel: Flutter view controller unavailable.")
+      NSLog("Unable to install native channels: Flutter view controller unavailable.")
       return
     }
 
-    backupExclusionChannel = BackupExclusionChannel(
-      binaryMessenger: controller.binaryMessenger
-    )
+    backupExclusionChannel = BackupExclusionChannel(binaryMessenger: controller.binaryMessenger)
+    nowPlayingChannel = NowPlayingChannel(binaryMessenger: controller.binaryMessenger)
   }
 }
