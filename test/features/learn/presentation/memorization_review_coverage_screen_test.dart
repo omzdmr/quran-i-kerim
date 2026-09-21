8782b86ed151bd9ca4f5c7432187c9b0c5cf1db3
omzdmr <154471764+omzdmr@users.noreply.github.com>
2026-09-22 02:06:11 +0800

    Fix Hifz coverage semantics test import

--- test/features/learn/presentation/memorization_review_coverage_screen_test.dart
+++ test/features/learn/presentation/memorization_review_coverage_screen_test.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' show SemanticsAction;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
